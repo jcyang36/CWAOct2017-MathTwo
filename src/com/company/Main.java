1f4e9c9fc46f0df539be0fb772be2cdb43259612
Joyce Yang <jcyang@hmc.edu>
2017-10-11 13:50:56 -0400

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int mynumber=2;
+        Double myothernumber=1.7938;
+        System.out.println("My number: " +mynumber);
+        System.out.println("My other number: "+myothernumber);
+    }
+}
